7333274c1cdc3e15bc4a937f35947591f525febf
uldana shyndali <122513545+shyndaliu@users.noreply.github.com>
2023-07-11 22:47:10 +0600

    changed file structure

---
+++ frontend/lib/authOptions.js
@@ -0,0 +1,16 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events',
+          authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code&state=hithere',
+        }
+      }
+    }),
+  ],
+};
