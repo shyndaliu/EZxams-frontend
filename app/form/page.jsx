bea01c0de0ed7967723fdc9f223a498fe680c078
uldana shyndali <122513545+shyndaliu@users.noreply.github.com>
2023-07-18 15:55:42 +0600

    made input for topics with rating

--- app/form/page.jsx
+++ app/form/page.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import DatePicker from '@/components/form/datetimepicker';
 import Balancer from "react-wrap-balancer";
 import TopicForm from '@/components/form/topicsform';
+import { SparklesIcon } from '@heroicons/react/20/solid';
 
 export default function Form() {
     const containerRef = useRef(null);
@@ -13,7 +14,7 @@ export default function Form() {
         if (element) {
             const elementRect = element.getBoundingClientRect();
             const absoluteElementTop = elementRect.top + window.scrollY;
-            const middle = absoluteElementTop - window.innerHeight / 2 + 100;
+            const middle = absoluteElementTop - window.innerHeight / 2 + 180;
             window.scrollTo({ top: middle, behavior: 'smooth' });
         }
     }
@@ -29,18 +30,14 @@ export default function Form() {
         }
     }
     return <>
-        <div className="w-full h-[4179px] relative">
-            <div className="w-full h-[4422px] absolute overflow-hidden">
+        <div className="w-full h-[3300px] relative">
+            <div className="w-full h-[3300px] absolute overflow-hidden">
                 <div className="right-[10px] top-[150px] absolute bg-blur-blue rounded-full w-[200px] h-[200px] blur-[100px] md:w-[400px] md:h-[400px] md:blur-[120px]" />
                 <div className="left-[10px] md:left-[267px] top-[2580px] absolute bg-blur-blue rounded-full w-[200px] h-[200px] blur-[100px] md:w-[535px] md:h-[518px] md:blur-[150px]" />
                 <div className="left-0 top-[350px] absolute bg-blur-pink rounded-full w-[200px] h-[200px] blur-[100px] md:w-[686px] md:h-[679px] md:blur-[150px]" />
-                <div className="w-[752px] h-[758px] left-[59px] top-[3800px] absolute bg-blur-pink rounded-full blur-[150px]" />
                 <div className="w-[531px] h-[515px] left-[1144px] top-[1700px] absolute bg-fuchsia-300 rounded-full blur-[150px]" />
             </div>
-            <motion.div initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: -15 }} transition={{ delay: 0.3, duration: 0.5 }} className="w-full h-[3900px] top-[100px] absolute overflow-hidden">
-                <div className="w-[464px] h-[496px] left-0 top-[3400px] absolute origin-top-left flex-col justify-center items-center inline-flex">
-                    <img className="w-[464px] h-[496px]" src="/form5.png" />
-                </div>
+            <motion.div initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: -15 }} transition={{ delay: 0.3, duration: 0.5 }} className="w-full h-[3200px] top-[100px] absolute overflow-hidden">
                 <div className="w-[486px] h-[633px] left-0 top-[1650px] absolute origin-top-left justify-center items-center inline-flex">
                     <img className="w-[486px] h-[633px]" src="/form3.png" />
                 </div>
@@ -73,29 +70,41 @@ export default function Form() {
                 <div className="bg-neutral-50 rounded-[29px] drop-shadow-xl flex flex-col justify-center w-[616px] px-10 pt-10 pb-5">
                     <p className="text-gray-800 text-5xl py-5">What is your deadline?</p>
                     <DatePicker></DatePicker>
-                    <div className='flex justify-end py-3'>
-                        <button className='px-4 py-1 border-gray-300 border-2 bg-btn/5 text-gray-800 text-xl rounded-3xl hover:bg-btn hover:text-white hover:border-btn transition-colors'
-                            onClick={() => scrollToElement('element2')}>
-                            next
-                        </button>
-                    </div>
+                    <button
+                        className="flex justify-center items-center w-full p-2 my-10 px-10 my-5 min-w-max text-sm text-center rounded-full backdrop-blur-sm transition-all md:text-base bg-blur-blue/20 hover:bg-blur-blue/80"
+                        onClick={() => scrollToElement('element2')}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true" height="30" className="inline text-white">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19 13l-7 7-7-7m14-8l-7 7-7-7"></path>
+                        </svg>
+                    </button>
                 </div>
             </div>
-
             <div id="element2" className="left-[533px] top-[1596px] absolute">
-                <TopicForm />
-            </div>
-
-            {/* <div id="element2" className="left-[533px] top-[1596px] absolute">
-                <div className="w-[616px] h-[272px] left-0 top-0 absolute">
-                    <div className="w-[616px] h-[272px] left-0 top-0 absolute bg-neutral-50 rounded-[29px] shadow" />
-                    <div className="w-[536px] h-[45px] left-[37px] top-[170px] absolute">
-                        <div className="w-[536px] h-[45px] left-0 top-0 absolute bg-gray-200 rounded-2xl" />
-                        <div className="left-[31px] top-[13px] absolute text-right text-zinc-700 text-[20px] font-normal leading-tight">Enter your name...</div>
-                    </div>
+                <div className="w-[700px] left-0 top-0 absolute bg-neutral-50 rounded-[29px] shadow flex flex-col justify-center py-5 px-5">
+                    <TopicForm />
+                    <button
+                        className="flex justify-center items-center w-full p-2 px-10 my-3 min-w-max text-sm text-center rounded-full backdrop-blur-sm transition-all md:text-base bg-blur-blue/20 hover:bg-blur-blue/80"
+                        onClick={() => scrollToElement('element3')}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true" height="30" className="inline text-white">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19 13l-7 7-7-7m14-8l-7 7-7-7"></path>
+                        </svg>
+                    </button>
                 </div>
-                <div className="w-[428px] h-[100px] left-[39px] top-[36px] absolute text-purple-900 text-[48px] font-semibold leading-10">First of all, how can i call you?</div>
-            </div> */}
+            </div>
+            <button id="element3" className="absolute top-[2800px] left-[450px] z-20  flex items-center justify-center space-x-5"
+            >
+                <div
+                    className="group flex items-center justify-center space-x-2 rounded-full border px-3 py-1 md:px-10 md:py-8  bg-btn text-sm text-white transition-colors hover:bg-back-white hover:text-btn hover:bg-opacity-5"
+                    rel="noopener noreferrer"
+                >
+
+                    <p className="text-xl md:text-5xl"> let's generate </p>
+                    <SparklesIcon className='w-10 h-10' />
+
+                </div>
+            </button>
         </div>
     </>
 }
--- components/form/topicsform.jsx
+++ components/form/topicsform.jsx
@@ -1,47 +1,94 @@
+import { StarIcon, TrashIcon } from '@heroicons/react/20/solid';
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 const TopicForm = () => {
     const [topics, setTopics] = useState({});
     const [newTopic, setNewTopic] = useState('');
+    const [hoveredTopic, setHoveredTopic] = useState(null);
 
     const handleAddTopic = () => {
         if (newTopic.trim() !== '') {
-            const updatedTopics = { ...topics, [newTopic]: 0 };
+            const updatedTopics = { [newTopic]: 0, ...topics };
             setTopics(updatedTopics);
             setNewTopic('');
             console.log(topics)
         }
     };
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleAddTopic();
+        }
+    };
+    const handleDeleteTopic = (topic) => {
+        // Add your logic to handle deleting the topic
+        console.log('Topic deleted:', topic);
+        const updatedTopics = { ...topics };
+        delete updatedTopics[topic];
+        setTopics(updatedTopics);
+    };
+
+    const handleRatingChange = (topic, rating) => {
+        const updatedTopics = { ...topics, [topic]: rating };
+        setTopics(updatedTopics);
+    };
+
+    const handleStarHover = (topic, index) => {
+        setHoveredTopic({ topic, index });
+    };
 
     return (
         <div>
-            <div>
+            <h1 className='text-gray-800 text-5xl py-5'>What topics you should prepare for?</h1>
+            <div className=''>
                 <input
+                    className='bg-gray-200 w-[80%] border-none rounded-2xl px-5px-4 py-1'
                     type="text"
                     placeholder="Enter a topic"
                     value={newTopic}
                     onChange={(e) => setNewTopic(e.target.value)}
+                    onKeyPress={handleKeyPress}
                 />
-                <button onClick={handleAddTopic}>Add</button>
+                <button className="px-4 py-1 mx-5 border-gray-300 border-2 bg-btn/5 text-gray-800 text-md rounded-3xl hover:bg-btn hover:text-white hover:border-btn transition-colors" onClick={handleAddTopic}>Add</button>
             </div>
-            {Object.entries(topics).map(([topic, rating]) => (
-                <div key={topic}>
-                    {topic}
-                    <input
-                        type="range"
-                        min="0"
-                        max="5"
-                        value={rating}
-                        onChange={(e) =>
-                            setTopics((prevTopics) => ({
-                                ...prevTopics,
-                                [topic]: parseInt(e.target.value),
-                            }))
-                        }
-                    />
-                    {rating}
-                </div>
-            ))}
+            <div id="topics" className='w-80% h-[200px] overflow-y-scroll overflow-x-hidden my-5 bg-gray-300/20 rounded-3xl text-xl py-2'>
+                {Object.entries(topics).map(([topic, rating]) => (
+                    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} key={topic} className='flex justify-between px-5 py-3'>
+                        <div id="topics" className='w-3/5 overflow-x-scroll bg-gray-800/10 rounded-xl py-1 px-1'>
+                            {topic}
+                        </div>
+                        <div className='w-2/5 flex justify-center items-center'>
+                            {[...Array(5)].map((_, i) => {
+                                const starColor =
+                                    (hoveredTopic &&
+                                        hoveredTopic.topic === topic &&
+                                        hoveredTopic.index >= i + 1) || (rating >= i + 1)
+                                        ? '#C1ABFF'
+                                        : 'gray';
+                                return (
+                                    <label
+                                        key={i}
+                                        onMouseEnter={() => handleStarHover(topic, i + 1)}
+                                        onMouseLeave={() => setHoveredTopic(null)}
+                                    >
+                                        <input
+                                            type="radio"
+                                            name={`rating-${topic}`}
+                                            value={i + 1}
+                                            checked={rating === i + 1}
+                                            onChange={() => handleRatingChange(topic, i + 1)}
+                                        />
+                                        <StarIcon className='w-5 h-5' style={{ color: starColor }} />
+                                    </label>
+                                );
+                            })}
+                        </div>
+                        <button onClick={() => handleDeleteTopic(topic)}><TrashIcon className='w-5 h-5 text-gray-500' /></button>
+                    </motion.div>
+
+                ))}
+            </div>
+            <h1 className='text-gray-500'>*please, rate how WELL you understand every topic in the scale from 1 to 5. it helps us to build a better plan just for you. thanks!</h1>
         </div>
     );
 };
