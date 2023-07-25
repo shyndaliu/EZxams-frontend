324a580a4c9d4b49070bc82a9e05a55784941421
uldana shyndali <122513545+shyndaliu@users.noreply.github.com>
2023-07-25 22:42:16 +0600

    deployment third try

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ["lh3.googleusercontent.com", "vercel.com"],
-  }
+  },
+  basePath: '/app',
+  pageExtensions: ['jsx'],
 };
 
 module.exports = nextConfig;
